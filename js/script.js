5091b6bbfb3334177475dcfdc3cd2dbc8406a462
Bhavyasree madineni <bhavyamadineni29@gmail.com>
2023-11-02 23:44:36 +0530

    portfolio

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*typing style*/
+
+var typed = new Typed(".typing", {
+    strings: ["Web Designer", "Web Developer", "Software Developer", "Programmer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+});
